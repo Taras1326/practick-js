7c6ebfde3991ce45c6c573599742c2f5a32462a8
Taras1326 <166243411+Taras1326@users.noreply.github.com>
2025-03-16 19:55:08 +0200

    loulu8o

---
+++ javas/thema2.js
@@ -0,0 +1,57 @@
+// const a = 24;
+
+// console.log(Number.isNaN(a));
+
+
+// const a = "24px";
+
+
+// console.log(a);
+
+// console.log(Number.parseInt(a));
+
+// const b = "24.5px";
+
+// console.log(Number.parseFloat(b));
+
+// const c = Number(prompt("ведіть число"));
+
+// console.log(c);
+
+// const d = 10===c;
+
+// console.log(d);
+
+
+// const f = Number("20")
+
+// console.log(f);
+
+// const h = Number("abc");
+
+// console.log(Number.isNaN(h));
+
+// console.log(0.1+0.2);
+
+// console.log((0.1 * 10 + 0.2 * 10)/ 10);
+
+// console.log(30 / 100);
+
+
+
+// console.log(Number.isNaN("5"));
+
+
+
+
+
+const age = 18;
+console.log(typeof(age));
+const message = Number(prompt("скільки вам років"));
+console.log(typeof(message));
+const rest = message === age;
+
+console.log(rest);
+
+
+
